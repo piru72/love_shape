88221b208419479e7d3ceaec6c485096a8dbbc5f
Parvez Ahammed <parvezdirom2000@gmail.com>
2021-02-09 02:41:46 +0600

    first commit

---
+++ love .py
@@ -0,0 +1,12 @@
+from turtle import*   #importing turtle function 
+color("pink")       #the color of the love shape
+begin_fill()        #strting to fill the shape
+left(50)            #from here start playing with the turtle function
+forward(100)
+circle(40, 180)
+left(260)
+circle(40,180)
+forward(100)
+end_fill()      #ending the shape fill
+done() 
+done()
